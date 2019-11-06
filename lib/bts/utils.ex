3d5655bb7ba4e30c46e1c9e8d473c3bb6b584c81
Kenton Wang <smartepsh@gmail.com>
2019-11-06 15:16:21 +0800

    feat: Utils.read_xlsx/2

--- lib/bts/utils.ex
+++ lib/bts/utils.ex
@@ -1,2 +1,14 @@
 defmodule BTS.Utils do
+  def read_xlsx(file_path, sheet \\ 0) do
+    {:ok, pid, parser} = Exoffice.parse(file_path, sheet)
+
+    data =
+      pid
+      |> Exoffice.get_rows(parser)
+      |> Enum.to_list()
+
+    Exoffice.close(pid, parser)
+
+    data
+  end
 end
